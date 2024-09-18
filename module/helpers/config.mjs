980b122b092c4dd3618423e520491a2c97d66cb8
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-09-18 14:07:59 +0200

    Add files via upload

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const HOW_TO_BE_A_HERO = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+HOW_TO_BE_A_HERO.abilities = {
+  str: 'HOW_TO_BE_A_HERO.Ability.Str.long',
+  dex: 'HOW_TO_BE_A_HERO.Ability.Dex.long',
+  con: 'HOW_TO_BE_A_HERO.Ability.Con.long',
+  int: 'HOW_TO_BE_A_HERO.Ability.Int.long',
+  wis: 'HOW_TO_BE_A_HERO.Ability.Wis.long',
+  cha: 'HOW_TO_BE_A_HERO.Ability.Cha.long',
+};
+
+HOW_TO_BE_A_HERO.abilityAbbreviations = {
+  str: 'HOW_TO_BE_A_HERO.Ability.Str.abbr',
+  dex: 'HOW_TO_BE_A_HERO.Ability.Dex.abbr',
+  con: 'HOW_TO_BE_A_HERO.Ability.Con.abbr',
+  int: 'HOW_TO_BE_A_HERO.Ability.Int.abbr',
+  wis: 'HOW_TO_BE_A_HERO.Ability.Wis.abbr',
+  cha: 'HOW_TO_BE_A_HERO.Ability.Cha.abbr',
+};
